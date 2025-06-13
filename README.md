b1014982ad3758631813e9a508866cd4ee01f160
giapre <g.preti98@gmail.com>
2025-06-13 13:13:46 +0200

    set up so it works with vbjax functions

---
+++ README.md
@@ -0,0 +1,20 @@
+**SUMMARY**
+
+In this work we show the use of a neural mass model equipped with D1 dopaminergic modulation of AMPA currents. This model is an extended version of the work of Gast et al (https://doi.org/10.1073/pnas.2311885121) that includes a dose-dependent neuromodulatory effect of dopamine, similarly to that of Depannemaecker et al (https://doi.org/10.1101/2024.06.23.600260). 
+
+This model simulates the brain as a network of neural mass models, integrating dopamine dynamics with glutamatergic and GABAergic synaptic interactions. It adapts a neural mass model to include the effects of dopamine via the D1 receptor, which enhances excitatory synaptic currents (AMPA and NMDA channels). Dopaminergic inputs and reuptake regulate dopamine concentration at each node. 
+The weights for these connections are derived from tractography data, adjusted based on connectivity studies. The whole-brain model can be implemented using three atlases, Desikan-Killiany (DK) and Schaefer 100 Parcels 7 Networks, and AAL2, with specific masks for glutamatergic, GABAergic, and dopaminergic projections. These projections are informed by established brain circuits using anatomical knowledge. D1 receptor numbers for brain regions are added using PET data, aligning receptor density values with each node in the model. 
+This approach allows for the simulation of neuromodulatory effects across different brain areas, with flexibility in adjusting network dynamics based on different parameterizations.  
+
+In this repository, the core mathematical model is implemented in gast_model.py, which includes equations for excitatory-only systems, as well as extended formulations that incorporate inhibitory and dopaminergic dynamics. Parameter values used in the equations are also provided within the same file.
+
+The utils.py module contains numerical integration routines based on the JAX library, adapted from vbjax (Marmaduke Woodman, Abolfazl Ziaeemehr, see doi: 10.5281/zenodo.14204249). It also includes utilities for simulating BOLD signals and computing Functional Connectivity Dynamics (FCD).
+
+The example.ipynb notebook demonstrates how to use the model in both single-node and whole-brain simulations. It begins by analyzing the dynamical repertoire of a single node across different input currents, illustrating the range of regimes the system can express. It then moves to full-network simulations, showcasing how to compute BOLD signals, structure-function connectivity (SCFC), and FCD. Finally, the notebook includes a brief parameter sweep over the excitatory connectivity weight (we), highlighting an optimal range where SCFC and FCD are maximized.
+
+The folder structural_data contains all the weights, connectivity masks, and receptor files for the three atlases.
+
+**For running the model**
+You will simply need to install jax in your python environment, and you can get started!
+
+**Please read the documentation.pdf file for a detailed description of the model and references.**
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 #!pip install vbjax
-#import vbjax as vb
-#DOI: 10.5281/zenodo.14204249
+#See at DOI: 10.5281/zenodo.14204249
 
 import time
 import os
@@ -10,228 +9,16 @@ import jax.tree_util
 import jax.numpy as jnp
 import numpy as np
 import numpy.random as nr
-
+import vbjax as vb
 import gast_model as gm
 
 #import matplotlib.pyplot as plt
 #from matplotlib.ticker import FuncFormatter
 
 """
-Functions for building time stepping loops.
-
-"""
-
-zero = 0
-tmap = jax.tree_util.tree_map
-
-def heun_step(x, dfun, dt, *args, add=zero, adhoc=None, return_euler=False):
-    """Use a Heun scheme to step state with a right hand sides dfun(.)
-    and additional forcing term add.
-    """
-    adhoc = adhoc or (lambda x,*args: x)
-    d1 = dfun(x, *args)
-    if add is not zero:
-        xi = tmap(lambda x,d,a: x + dt*d + a, x, d1, add)
-    else:
-        xi = tmap(lambda x,d: x + dt*d, x, d1)
-    xi = adhoc(xi, *args)
-    d2 = dfun(xi, *args)
-    if add is not zero:
-        nx = tmap(lambda x,d1,d2,a: x + dt*0.5*(d1 + d2) + a, x, d1, d2, add)
-    else:
-        nx = tmap(lambda x,d1,d2: x + dt*0.5*(d1 + d2), x, d1, d2)
-    nx = adhoc(nx, *args)
-    if return_euler:
-        return xi, nx
-    return nx
-
-
-def _compute_noise(gfun, x, p, sqrt_dt, z_t):
-    g = gfun(x, p)
-    try: # maybe g & z_t are just arrays
-        noise = g * sqrt_dt * z_t
-    except TypeError: # one of them is a pytree
-        if isinstance(g, float): # z_t is a pytree, g is a scalar
-            noise = tmap(lambda z: g * sqrt_dt * z, z_t)
-        # otherwise, both must be pytrees and they must match
-        elif not jax.tree_util.tree_all(jax.tree_util.tree_structure(g) == 
-                                        jax.tree_util.tree_structure(z_t)):
-            raise ValueError("gfun and z_t must have the same pytree structure.")
-        else:
-            noise = tmap(lambda g,z: g * sqrt_dt * z, g, z_t)
-    return noise
-
-def make_sde(dt, dfun, gfun, adhoc=None, return_euler=False, unroll=10):
-    """Use a stochastic Heun scheme to integrate autonomous stochastic
-    differential equations (SDEs).
-
-    Parameters
-    ==========
-    dt : float
-        Time step
-    dfun : function
-        Function of the form `dfun(x, p)` that computes drift coefficients of
-        the stochastic differential equation.
-    gfun : function or float
-        Function of the form `gfun(x, p)` that computes diffusion coefficients
-        of the stochastic differential equation. If a numerical value is
-        provided, this is used as a constant diffusion coefficient for additive
-        linear SDE.
-    adhoc : function or None
-        Function of the form `f(x, p)` that allows making adhoc corrections
-        to states after a step.
-    return_euler: bool, default False
-        Return solution with local Euler estimates.
-    unroll: int, default 10
-        Force unrolls the time stepping loop.
-
-    Returns
-    =======
-    step : function
-        Function of the form `step(x, z_t, p)` that takes one step in time
-        according to the Heun scheme.
-    loop : function
-        Function of the form `loop(x0, zs, p)` that iteratively calls `step`
-        for all `z`.
-
-    Notes
-    =====
-
-    In both cases, a Jax compatible parameter set `p` is provided, either an array
-    or some pytree compatible structure.
-
-    Note that the integrator does not sample normally distributed noise, so this
-    must be provided by the user.
-
-
-    >>> import vbjax as vb
-    >>> _, sde = vb.make_sde(1.0, lambda x, p: -x, 0.1)
-    >>> sde(1.0, vb.randn(4), None)
-    Array([ 0.5093468 ,  0.30794007,  0.07600437, -0.03876263], dtype=float32)
-
-    """
-
-    sqrt_dt = jnp.sqrt(dt)
-
-    # gfun is a numerical value or a function f(x,p) -> sig
-    if not hasattr(gfun, '__call__'):
-        sig = gfun
-        gfun = lambda *_: sig
-
-    def step(x, z_t, p):
-        noise = _compute_noise(gfun, x, p, sqrt_dt, z_t)
-        return heun_step(
-            x, dfun, dt, p, add=noise, adhoc=adhoc,
-            return_euler=return_euler)
-
-    @jax.jit
-    def loop(x0, zs, p):
-        def op(x, z):
-            x = step(x, z, p)
-            # XXX gets unwieldy, how to improve?
-            if return_euler:
-                ex, x = x
-            else:
-                ex = None
-            return x, (ex, x)
-        _, xs = jax.lax.scan(op, x0, zs, unroll=unroll)
-        if not return_euler:
-            _, xs = xs
-        return xs
-
-    return step, loop
-
-def make_offline(step_fn, sample_fn, *args):
-    "Compute monitor samples in an offline or batch fashion."
-    def op(mon, x):
-        mon = step_fn(mon, x)
-        return mon, None
-    def offline_sample(mon, xs):
-        mon, _ = jax.lax.scan(op, mon, xs)
-        mon, samp = sample_fn(mon)
-        return mon, samp
-    return offline_sample
-
-# NB shape here is the input shape of neural activity
-
-def make_timeavg(shape):
-    "Make a time average monitor."
-    new = lambda : {'y': jnp.zeros(shape), 'n': 0}
-    def step(buf, x):
-        return {'y': buf['y'] + x,
-                'n': buf['n'] + 1}
-    def sample(buf):
-        return new(), buf['y'] / buf['n']
-    return new(), step, sample
-
-
-def make_gain(gain, shape=None):
-    "Make a gain-matrix monitor suitable for sEEG, EEG & MEG."
-    tavg_shape = gain.shape[:1] + (shape[1:] if shape else ())
-    buf, tavg_step, tavg_sample = make_timeavg(tavg_shape)
-    step = lambda b, x: tavg_step(b, gain @ x)
-    return buf, step, tavg_sample
-
-
-# Bold implementation 
-
-BOLDTheta = collections.namedtuple(
-    typename='BOLDTheta',
-    field_names='tau_s,tau_f,tau_o,alpha,te,v0,e0,epsilon,nu_0,'
-                'r_0,recip_tau_s,recip_tau_f,recip_tau_o,recip_alpha,'
-                'recip_e0,k1,k2,k3'
-)
-
-def compute_bold_theta(
-        tau_s=0.65,
-        tau_f=0.41,
-        tau_o=0.98,
-        alpha=0.32,
-        te=0.04,
-        v0=4.0,
-        e0=0.4,
-        epsilon=0.5,
-        nu_0=40.3,
-        r_0=25.0,
-    ):
-    recip_tau_s = 1.0 / tau_s
-    recip_tau_f = 1.0 / tau_f
-    recip_tau_o = 1.0 / tau_o
-    recip_alpha = 1.0 / alpha
-    recip_e0 = 1.0 / e0
-    k1 = 4.3 * nu_0 * e0 * te
-    k2 = epsilon * r_0 * e0 * te
-    k3 = 1.0 - epsilon
-    return BOLDTheta(**locals())
-
-bold_default_theta = compute_bold_theta()
-
-def bold_dfun(sfvq, x, p: BOLDTheta):
-    s, f, v, q = sfvq
-    ds = x - p.recip_tau_s * s - p.recip_tau_f * (f - 1)
-    df = s
-    dv = p.recip_tau_o * (f - v ** p.recip_alpha)
-    dq = p.recip_tau_o * (f * (1 - (1 - p.e0) ** (1 / f)) * p.recip_e0
-                          - v ** p.recip_alpha * (q / v))
-    return jnp.array([ds, df, dv, dq])
-
-def make_bold(shape, dt, p: BOLDTheta):
-    "Make a BOLD fMRI monitor."
-    sfvq = jnp.ones((4,) + shape)
-    sfvq = sfvq.at[0].set(0)
-    def step(sfvq, x):
-        return heun_step(sfvq, bold_dfun, dt, x, p)
-    def sample(buf):
-        s, f, v, q = buf
-        return buf, p.v0 * (p.k1*(1 - q) + p.k2*(1 - q / v) + p.k3*(1 - v))
-    return sfvq, step, sample
-
-"""
 Functions for computing the BOLD signal
 """
 
-# the `make_run` function just needs dimensions of the problem 
-# the `make_run` function just needs dimensions of the problem 
 def make_run(init, tavg_period, total_time, dt, sigma, adhoc):
     '''returns the run function;
     so it basically serves as a factory function that creates
@@ -241,18 +28,18 @@ def make_run(init, tavg_period, total_time, dt, sigma, adhoc):
     nta = int(np.ceil(total_time / tavg_period)) # number of chunk
 
     noise_intensity = jnp.array([0., 0.1, 0.01, 0., 0., 0., 0.]).reshape(-1,1)*sigma
-    _, loop = make_sde(dt, gm.dopa_net, noise_intensity, adhoc)
+    _, loop = vb.make_sde(dt, gm.dopa_net, noise_intensity, adhoc)
 
     # setup tavg monitor
     n_nodes = init.shape[1]
-    ta_buf, ta_step, ta_sample = make_timeavg((init.shape[0], n_nodes))
-    ta_sample = make_offline(ta_step, ta_sample)
+    ta_buf, ta_step, ta_sample = vb.make_timeavg((init.shape[0], n_nodes))
+    ta_sample = vb.make_offline(ta_step, ta_sample)
 
     # now setup bold
-    bold_buf, bold_step, bold_samp = make_bold(
+    bold_buf, bold_step, bold_samp = vb.make_bold(
         shape=init[0].shape,  # only r
         dt=tavg_period/1e3, #trick: 10*
-        p=bold_default_theta)
+        p=vb.bold_default_theta)
 
     # run function actually does the simulation based on inputs
     # that we might want to sweep over or change
@@ -295,8 +82,8 @@ def make_run(init, tavg_period, total_time, dt, sigma, adhoc):
 def run_for_parameters(p, key=jax.random.PRNGKey(0), plot=False) :
     
     """
-    Takes into argument the (global coupling, MPR parameters, SC matrix) and random generation key
-    Returns the downsampled BOLD, FC, and FCD
+    Takes into argument the (global coupling, model parameters, SC matrix) and random generation key
+    Returns the raw temporal average and the downsampled BOLD
     """
     start = time.time()
 
